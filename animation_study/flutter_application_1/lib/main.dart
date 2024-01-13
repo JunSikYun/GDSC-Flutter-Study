0061d2618e277b507055970787a29b693bb4c226
JunSikYun <98214965+JunSikYun@users.noreply.github.com>
2024-01-14 05:42:51 +0900

    AnimationController

--- animation_study/flutter_application_1/lib/main.dart
+++ animation_study/flutter_application_1/lib/main.dart
@@ -1,43 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: WeatherApp(),
+      home: FadeDemo(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class WeatherApp extends StatefulWidget {
+class FadeDemo extends StatefulWidget {
   @override
-  _WeatherAppState createState() => _WeatherAppState();
+  _FadeDemoState createState() => _FadeDemoState();
 }
 
-class _WeatherAppState extends State<WeatherApp> with SingleTickerProviderStateMixin {
+class _FadeDemoState extends State<FadeDemo> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _animation;
-  String _weatherState = 'Sunny'; // 날씨 상태 (예: Sunny, Cloudy)
-  double _temperature = 25.0; // 온도
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: const Duration(seconds: 3),
+      duration: const Duration(seconds: 1),
       vsync: this,
     );
 
-    _animation = CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut,
-    );
+    _animation = Tween<double>(begin: 1.0, end: 0.0).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      });
 
-    _controller.forward();
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        _controller.forward();
+      }
+    });
   }
 
   @override
@@ -50,51 +53,26 @@ class _WeatherAppState extends State<WeatherApp> with SingleTickerProviderStateM
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Weather App'),
+        title: Text('AnimationController Test'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$_weatherState',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            FadeTransition(
-              opacity: _animation,
-              child: WeatherIcon(weatherState: _weatherState),
-            ),
-            Text(
-              '${_temperature.toStringAsFixed(1)}°C',
-              style: Theme.of(context).textTheme.headline3,
-            ),
-          ],
+        child: FadeTransition(
+          opacity: _animation,
+          child: Image.network(
+            'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+          ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (_controller.isAnimating) {
+            _controller.stop();
+          } else {
+            _controller.forward();
+          }
+        },
+        child: Icon(Icons.play_arrow),
+      ),
     );
-  }
-}
-
-class WeatherIcon extends StatelessWidget {
-  final String weatherState;
-
-  WeatherIcon({required this.weatherState});
-
-  @override
-  Widget build(BuildContext context) {
-    IconData iconData;
-    switch (weatherState) {
-      case 'Cloudy':
-        iconData = Icons.cloud;
-        break;
-      case 'Rainy':
-        iconData = Icons.grain;
-        break;
-      // 기타 날씨 상태에 따른 아이콘 추가 가능
-      default: // 'Sunny' 등 기타 상태
-        iconData = Icons.wb_sunny;
-        break;
-    }
-    return Icon(iconData, size: 100);
   }
 }
